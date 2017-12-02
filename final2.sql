6650f234bc5b1f0f2e99060abe973c4769540c7b
Sergey-Akmalov <akmolgt@gmail.com>
2017-12-03 00:25:58 +0300

    dump db

---
+++ final2.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Дек 03 2017 г., 00:24
+-- Версия сервера: 5.7.19
+-- Версия PHP: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `final2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` smallint(255) NOT NULL,
+  `login` char(64) NOT NULL,
+  `password` char(255) NOT NULL,
+  `name` char(100) DEFAULT NULL,
+  `age` tinyint(4) DEFAULT NULL,
+  `description` char(255) DEFAULT NULL,
+  `photo` char(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `name`, `age`, `description`, `photo`) VALUES
+(3, 'Mary', '$2y$10$IUAjJF4lamwyYWdqNWw3a.KdJ4XxfY37CHfwqlkKsMJpaPdMNxTB6', 'Mary Elizabeth Winstead', 33, '', '3.jpg?v=3'),
+(4, 'sienna', '$2y$10$IUAjJF4lamwyYWdqNWw3a.jg9MIX2ep6s7Vq87/BYKghu3hcRqV7S', 'Sienna Miller', 35, '', '4.jpg?v=0');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` smallint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
